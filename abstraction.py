e9fa3c6203f15945ab1e4b9389c59793acc6a660
Justin P <jadamsperry@gmail.com>
2020-07-05 11:25:00 -0600

    abstraction update using self
    missed a self. in my code, now can use passed arguments

--- abstraction.py
+++ abstraction.py
@@ -3,10 +3,11 @@ from abc import ABC, abstractmethod
 # define a parent class
 
 class loan(ABC):
+    l_amount = 5000
     def loan_amount(self, l_amount):
-        loan = l_amount
+##        loan = l_amount
         print('Your owe {} on your car..'.format(l_amount))
-        return loan
+        return l_amount
 
     def monthly_payment(self, pay_amount):
         print('Your monthly payment is: {}'.format(pay_amount))
@@ -23,8 +24,8 @@ class leased_suv(loan):
 
     # abstract parent class defined
     def payment_actual(self, act_amount):
-        loan = 5000
-        total_amount = 5000 - act_amount
+##        loan = 5000
+        total_amount = self.l_amount - act_amount
         print('After your monthly payment of {} this month, you still owe {} on the rav4.'.format(act_amount, total_amount))
 
 if __name__ == '__main__':
@@ -32,8 +33,3 @@ if __name__ == '__main__':
     money.loan_amount(5000)
     money.monthly_payment(300)
     money.payment_actual(100)
-    
-        
-        
-        
-        
