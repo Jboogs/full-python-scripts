4afc39b51cbc7c6d8e02e1979966c604d887b6d1
Justin P <jadamsperry@gmail.com>
2020-08-02 21:21:46 -0600

    change dir layout

---
+++ sqlite_apps/sqlite3_assignment_1.py
@@ -0,0 +1,14 @@
+#
+#   Python version: 3.8.3
+#
+#   Author: Justin Perry
+#
+#   To demonstrate the SQLite3 module and Python to build 
+#   and work on databases
+#
+#
+
+
+# Import SQLite3 module
+import sqlite3
+connection = sqlite3.connect('
